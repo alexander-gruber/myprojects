dfa1dcd49502bb7e14557bd4949016c1b807af23
Alexander Gruber <agruber@geo.tuwien.ac.at>
2023-10-18 16:57:32 +0200

    updates

--- experiments/observability/drought.py
+++ experiments/observability/drought.py
@@ -12,59 +12,74 @@ import matplotlib.pyplot as plt
 #  p(sm < Y   | x) = \int(-inf, Y)    p(x | sm) * p(sm) / p(x) dsm
 #  p(sm < inf | x) = \int (-inf, inf) p(x | sm) * p(sm) / p(x) dsm
 
-# f(sm,x) dsm dx
+
 def func(sm, x, sig_sm, sig_err):
+    """
+    Bayese rule applied to derive p(drought | measurement) = p(measurement|drought) * p(drought) / p(measurement)
+    """
 
     sig_meas = np.sqrt(sig_sm ** 2 + sig_err ** 2)
     return norm.pdf(x, loc=sm, scale=sig_err) * norm.pdf(sm, loc=0, scale=sig_sm) / norm.pdf(x, loc=0, scale=sig_meas)
 
-def plot_prob_vs_threshold(threshold=0.3):
+# def plot_prob_vs_threshold(threshold=0.3):
+#
+#     sig_sm = 1
+#     ninf = norm.ppf(0)
+#     # pinf = norm.ppf(1)
+#     thres = norm.ppf(threshold)
+#
+#     xs = np.linspace(norm.ppf(0.01), 0, 20)
+#     SNRs = [f'{snr:.2f}' for snr in np.geomspace(1, 5, 5)]
+#
+#     prob = pd.DataFrame(index=xs, columns=SNRs)
+#     prob.columns.name = "SNR"
+#
+#     for SNR in SNRs:
+#         sig_err = 1 / float(SNR)
+#         for x in xs:
+#             p = quad(func, ninf, thres, args=(x, sig_sm, sig_err), limit=100)
+#             # n = quad(func, ninf, pinf, args=(x, sig_sm, sig_err), limit=100) # normalization likely not necessary
+#             prob.loc[x, SNR] = p[0] #/ n[0]
+#
+#     fig, ax = plt.subplots()
+#     prob.plot(ax=ax)
+#     ax.set_xlabel('Drought thresholds')
+#     ax.set_ylabel(f'Probability of drought')
+#     plt.axvline(norm.ppf(0.3), color='black', linestyle='--', linewidth=1)
+#     plt.axvline(norm.ppf(0.2), color='black', linestyle='--', linewidth=1)
+#     plt.axvline(norm.ppf(0.1), color='black', linestyle='--', linewidth=1)
+#     plt.axvline(norm.ppf(0.05), color='black', linestyle='--', linewidth=1)
+#     plt.axvline(norm.ppf(0.02), color='black', linestyle='--', linewidth=1)
+#     plt.axhline(0.95, color='black', linestyle=':', linewidth=1.5)
+#
+#     plt.tight_layout()
+#     plt.show()
 
-    sig_sm = 1
-    ninf = norm.ppf(0)
-    # pinf = norm.ppf(1)
-    thres = norm.ppf(threshold)
+def get_drought_thresholds():
+    """
+    Plot the probability of detecting at least a light drought as a function of the SNR
+    """
 
-    xs = np.linspace(norm.ppf(0.01), 0, 20)
-    SNRs = [f'{snr:.2f}' for snr in np.geomspace(1, 5, 5)]
+    # derive equidistant drought classes
+    dt = norm.ppf(0.3) - norm.ppf(0.2)
+    return [norm.cdf(norm.ppf(0.3) - i * dt) for i in np.arange(6)][::-1]
 
-    prob = pd.DataFrame(index=xs, columns=SNRs)
-    prob.columns.name = "SNR"
-
-    for SNR in SNRs:
-        sig_err = 1 / float(SNR)
-        for x in xs:
-            p = quad(func, ninf, thres, args=(x, sig_sm, sig_err), limit=100)
-            # n = quad(func, ninf, pinf, args=(x, sig_sm, sig_err), limit=100) # normalization likely not necessary
-            prob.loc[x, SNR] = p[0] #/ n[0]
-
-    fig, ax = plt.subplots()
-    prob.plot(ax=ax)
-    ax.set_xlabel('Drought thresholds')
-    ax.set_ylabel(f'Probability of drought')
-    plt.axvline(norm.ppf(0.3), color='black', linestyle='--', linewidth=1)
-    plt.axvline(norm.ppf(0.2), color='black', linestyle='--', linewidth=1)
-    plt.axvline(norm.ppf(0.1), color='black', linestyle='--', linewidth=1)
-    plt.axvline(norm.ppf(0.05), color='black', linestyle='--', linewidth=1)
-    plt.axvline(norm.ppf(0.02), color='black', linestyle='--', linewidth=1)
-    plt.axhline(0.95, color='black', linestyle=':', linewidth=1.5)
-
-    plt.tight_layout()
-    plt.show()
-
-def plot_prob_vs_snr():
+def plot_light_drought_detection_probability():
+    """
+    Plot the probability of detecting at least a light drought as a function of the SNR
+    """
 
     SNRs = [float(f'{snr:.4f}') for snr in np.geomspace(1, 30, 20)]
     cols = ['exceptional', 'extreme', 'severe', 'moderate', 'slight']
     prob = pd.DataFrame(index=SNRs, columns=cols)
     prob.columns.name = "Observed magnitude"
 
-    dt = norm.ppf(0.3) - norm.ppf(0.2)
-    thresholds = [norm.cdf(norm.ppf(0.3) - i * dt) for i in np.arange(6)][::-1]
+    thresholds = get_drought_thresholds()
 
+    # Derive detection probability for each class
     for SNR in SNRs:
         for i, col in zip(np.arange(1, len(thresholds)), cols):
-            prob.loc[SNR, col] = get_prob(SNR, thresholds[i-1], thresholds[i])
+            prob.loc[SNR, col] = get_prob(SNR, thresholds[i-1], thresholds[i], drought_thres=thresholds[-1])
 
     fig, ax = plt.subplots()
     prob.plot(ax=ax)
@@ -74,44 +89,56 @@ def plot_prob_vs_snr():
 
     plt.axhline(0.95, color='k', linewidth=1, linestyle='--')
 
+    # Thresholds obtained from "get_detectability_threshold()"
     plt.axvline(25.4792, color='k', linewidth=1, linestyle='--')
     plt.axvline(3.8027, color='k', linewidth=1, linestyle='--')
     plt.axvline(2.2591, color='k', linewidth=1, linestyle='--')
     plt.axvline(1.6306, color='k', linewidth=1, linestyle='--')
     plt.axvline(1.2854, color='k', linewidth=1, linestyle='--')
-    # plt.axvline(25.72128010004531, color='k', linewidth=1, linestyle='--')
-    # plt.axvline(3.835335992849183, color='k', linewidth=1, linestyle='--')
-    # plt.axvline(2.453779526426614, color='k', linewidth=1, linestyle='--')
-    # plt.axvline(1.6350673480580982, color='k', linewidth=1, linestyle='--')
-    # plt.axvline(1.567825507750969, color='k', linewidth=1, linestyle='--')
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_drought_severity_detection_probability():
+    """
+    Plot the probability of detecting at least a light drought as a function of the SNR
+    """
+
+    SNRs = [float(f'{snr:.4f}') for snr in np.geomspace(1, 30, 20)]
+    cols = ['exceptional', 'extreme', 'severe', 'moderate', 'slight']
+    prob = pd.DataFrame(index=SNRs, columns=cols)
+    prob.columns.name = "Observed magnitude"
+
+    thresholds = get_drought_thresholds()
+
+    # Derive detection probability for each class
+    for SNR in SNRs:
+        for i, col in zip(np.arange(1, len(thresholds)), cols):
+            prob.loc[SNR, col] = get_prob(SNR, thresholds[i-1], thresholds[i], drought_thres=thresholds[i])
+
+    fig, ax = plt.subplots()
+    prob.plot(ax=ax)
+    ax.set_xlabel('SNR')
+    ax.set_xscale('log')
+    ax.set_ylabel(f'Probability of drought severity greater or equal than observed')
+
+    plt.axhline(0.5, color='k', linewidth=1, linestyle='--')
+
+    # Thresholds obtained from "get_detectability_threshold()"
+    plt.axvline(1.1977, color='k', linewidth=1, linestyle='--')
+    plt.axvline(1.5573, color='k', linewidth=1, linestyle='--')
+    plt.axvline(1.8502, color='k', linewidth=1, linestyle='--')
+    plt.axvline(2.1005, color='k', linewidth=1, linestyle='--')
+    plt.axvline(2.3255, color='k', linewidth=1, linestyle='--')
 
     plt.tight_layout()
     plt.show()
 
 
-def minimize(fct, args, lower, upper):
-
-    res = 1
-    while res > 0.005:
-        # print(f'res: {res}')
-        tmp_diff = 1
-        xs = np.geomspace(lower, upper, 5)
-
-        for i, x in enumerate(xs):
-            # print(f'diff: {tmp_diff}')
-            diff = abs(0.95-fct(x, *args))
-            if diff > tmp_diff:
-                lower = xs[i-1]
-                upper = x
-                res = abs(x-xs[i-1])
-                break
-            else:
-                tmp_diff = diff
-
-    return x
-
-
-def get_prob(SNR, lower, upper):
+def get_prob(SNR, lower, upper, drought_thres):
+    """
+    Obtain the probability that measurements between an interval of the pdf indicate a drought
+    """
 
     sig_sm = 1
     sig_err = 1 / float(SNR)
@@ -119,38 +146,52 @@ def get_prob(SNR, lower, upper):
 
     t_l = norm.ppf(lower, scale=sig_meas)
     t_u = norm.ppf(upper, scale=sig_meas)
+    thres =  norm.ppf(drought_thres)
 
-    return dblquad(func, t_l, t_u, -np.inf, norm.ppf(0.3), args=(sig_sm, sig_err))[0] / (t_u - t_l)
+    # integrate over f(sm, meas) dsm dmeas ; args = meas_lower, meas_upper, sm_lower, sm_upper
+    return dblquad(func, t_l, t_u, -np.inf, thres, args=(sig_sm, sig_err))[0] / (t_u - t_l)
 
 
-def get_detectability_threshold():
+def get_detectability_threshold(light=True):
+    """
+    Find the minimum SNR for which the probability of detection exceeds 95% for a given drought class
+    """
 
-    dt = norm.ppf(0.3) - norm.ppf(0.2)
-    thresholds = [norm.cdf(norm.ppf(0.3)-i*dt) for i in np.arange(6)]
-    # classes = ['slight', 'moderate', 'severe', 'extreme', 'exceptional']
+    # derive equidistant drought classes
+    thresholds = get_drought_thresholds()[::-1]
+    classes = ['slight', 'moderate', 'severe', 'extreme', 'exceptional', 'undetectable']
 
-    for i in np.arange(len(thresholds)-1):
+    # iterate over thesholds
+    for i, cl in zip(np.arange(len(thresholds)-1), classes):
         t_u = thresholds[i]
         t_l = thresholds[i+1]
 
-        if i==0:
-            bounds=[25,28] # slight
+
+        if light:
+            drought_thres = thresholds[0]
+            prob_thres = 0.95
+            if i==0:
+                bounds=[25,28] # slight
+            else:
+                bounds=[1,6] # > slight
         else:
-            bounds=[1,6] # > slight
+            drought_thres = t_u
+            prob_thres = 0.5
+            bounds=[0,6]
 
-        # res = minimize_scalar(get_prob, method='Bounded', bounds=bounds, args=(t_l, t_u), tol=0.01)
-        res = minimize(get_prob, (t_l, t_u), bounds[0], bounds[1])
+        min_get_prob = lambda *args: np.abs(prob_thres-get_prob(*args))
+        res = minimize_scalar(min_get_prob, method='Bounded', bounds=bounds, args=(t_l, t_u, drought_thres), tol=0.01)
 
-        print(res)
+        print(f'{cl}: {res.x:.6f}')
 
 
 if __name__=='__main__':
 
     # original thresholds: [0.01, 0.05, 0.1, 0.2, 0.3]
-
     # print([f'{norm.cdf(t):.2f}' for t in thresholds])
     # equidistant thresholds: ['0.30', '0.20', '0.12', '0.07', '0.04', '0.02']
 
-    plot_prob_vs_snr()
+    # plot_light_drought_detection_probability()
+    plot_drought_severity_detection_probability()
 
-    # get_detectability_threshold()
+    # get_detectability_threshold(light=False)
--- experiments/observability/plot_SMOS_predictability.py
+++ experiments/observability/plot_SMOS_predictability.py
@@ -1,3 +1,5 @@
+import platform
+
 import numpy as np
 import pandas as pd
 
@@ -12,13 +14,18 @@ import cartopy.feature as cfeature
 import cartopy.crs as ccrs
 
 
-def plot_smos_predictability():
+def plot_min_detection_threshold():
 
     avg_factor = np.sqrt(10)
     th = [25.4792, 3.8027, 2.2591, 1.6306, 1.2854]
-    cl = ['slight', 'moderate', 'severe', 'extreme', 'exceptional', 'unpredictable']
+    cl = ['slight', 'moderate', 'severe', 'extreme', 'exceptional', 'undetectable']
 
-    with Dataset('/Users/ag/Downloads/0-ERA5.swvl1_with_1-ASCAT.sm_with_2-SMOS_L2.Soil_Moisture.nc') as ds:
+    if platform.system() == 'Darwin':
+        fname = '/Users/ag/Downloads/0-ERA5.swvl1_with_1-ASCAT.sm_with_2-SMOS_L2.Soil_Moisture.nc'
+    else:
+        fname = r"H:\work\experiments\drought_predictability\0-ERA5.swvl1_with_1-ASCAT.sm_with_2-SMOS_L2.Soil_Moisture.nc"
+
+    with Dataset(fname) as ds:
         lats = ds['lat'][:].data
         lons = ds['lon'][:].data
         snr_ascat = ds['snr_1-ASCAT_between_0-ERA5_and_1-ASCAT_and_2-SMOS_L2'][:].data
@@ -35,21 +42,89 @@ def plot_smos_predictability():
     img_ascat[latidx, lonidx] = np.sqrt(10**(snr_ascat/10)) * avg_factor
     img_smos[latidx, lonidx] = np.sqrt(10**(snr_smos/10)) * avg_factor
 
-    img2_ascat = img_ascat.copy()
-    img2_smos = img_ascat.copy()
-
-    for i in np.arange(len(th)-1):
-        if i==0:
-            img2_ascat[img_ascat>=th[i]] = i
-            img2_smos[img2_smos>=th[i]] = i
-        else:
-            img2_ascat[(img_ascat >= th[i])&(img_ascat < th[i-1])] = i
-            img2_smos[(img2_smos >= th[i])&(img2_smos < th[i-1])] = i
-    img2_ascat[img_ascat < th[i]] = i+1
-    img2_smos[img2_smos < th[i]] = i+1
+    img2_ascat = np.full(img_ascat.shape, np.nan)
+    img2_smos = np.full(img_smos.shape, np.nan)
+    img2_ascat[img_ascat <= th[0]] = 0
+    img2_smos[img_smos <= th[0]] = 0
+    for i in np.arange(len(th)):
+        img2_ascat[img_ascat <= th[i]] = i+1
+        img2_smos[img_smos <= th[i]] = i+1
 
     fig = plt.figure(figsize=(14, 4))
     fig.suptitle('Minimum required severity for reliable drought detection (monthly average)')
+    extent = [lons.min(), lons.max(), lats.min(), lats.max()]
+    # cmap = 'viridis'
+    cmap = get_cmap("magma_r", len(th)+1)
+    lim = [0, len(th)+1]
+    title1 = 'ASCAT'
+    title2 = 'SMOS'
+
+    ax = fig.add_subplot(1, 2, 1, projection=ccrs.PlateCarree())
+    ax.set_title(title1)
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.LAND, alpha=0.5)
+    ax.add_feature(cfeature.BORDERS, linewidth=0.5)
+    img = ax.imshow(np.flipud(img2_ascat), extent=extent, cmap=cmap, vmin=lim[0], vmax=lim[1],
+                    transform=ccrs.PlateCarree())
+    # cbar = fig.colorbar(img, orientation='vertical', label=cmap_label, ticks=np.arange(len(cl))+0.5)
+    # cbar.ax.set_yticklabels(cl)
+
+    ax = fig.add_subplot(1, 2, 2, projection=ccrs.PlateCarree())
+    ax.set_title(title2)
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.LAND, alpha=0.5)
+    ax.add_feature(cfeature.BORDERS, linewidth=0.5)
+    img = ax.imshow(np.flipud(img2_smos), extent=extent, cmap=cmap, vmin=lim[0], vmax=lim[1],
+                    transform=ccrs.PlateCarree())
+    cbar = fig.colorbar(img, ax=fig.axes, orientation='vertical', ticks=np.arange(len(cl))+0.5)
+    cbar.ax.set_yticklabels(cl)
+
+    # plt.tight_layout()
+    # plt.show()
+
+    fout = r"H:\work\experiments\drought_predictability\detection_probability_thresholds.png"
+    fig.savefig(fout, dpi=300, bbox_inches='tight')
+    plt.close()
+
+def plot_most_likely_threshold():
+
+    avg_factor = np.sqrt(10)
+    th = [1.1977, 1.5573, 1.8502, 2.1005, 2.3255]
+    cl = ['undetectable', 'slight', 'moderate', 'severe', 'extreme', 'exceptional']
+
+    if platform.system() == 'Darwin':
+        fname = '/Users/ag/Downloads/0-ERA5.swvl1_with_1-ASCAT.sm_with_2-SMOS_L2.Soil_Moisture.nc'
+    else:
+        fname = r"H:\work\experiments\drought_predictability\0-ERA5.swvl1_with_1-ASCAT.sm_with_2-SMOS_L2.Soil_Moisture.nc"
+
+    with Dataset(fname) as ds:
+        lats = ds['lat'][:].data
+        lons = ds['lon'][:].data
+        snr_ascat = ds['snr_1-ASCAT_between_0-ERA5_and_1-ASCAT_and_2-SMOS_L2'][:].data
+        snr_smos = ds['snr_2-SMOS_L2_between_0-ERA5_and_1-ASCAT_and_2-SMOS_L2'][:].data
+
+    latarr = np.arange(lats.min(), lats.max()+0.25, 0.25)
+    lonarr = np.arange(lons.min(), lons.max()+0.25, 0.25)
+
+    latidx = ((lats - lats.min())/0.25).astype('int')
+    lonidx = ((lons - lons.min())/0.25).astype('int')
+
+    img_ascat = np.full((len(latarr), len(lonarr)), np.nan)
+    img_smos = np.full((len(latarr), len(lonarr)), np.nan)
+    img_ascat[latidx, lonidx] = np.sqrt(10**(snr_ascat/10)) * avg_factor
+    img_smos[latidx, lonidx] = np.sqrt(10**(snr_smos/10)) * avg_factor
+
+    img2_ascat = np.full(img_ascat.shape, np.nan)
+    img2_smos = np.full(img_smos.shape, np.nan)
+    img2_ascat[img_ascat < th[0]] = 0
+    img2_smos[img_smos < th[0]] = 0
+    for i in np.arange(len(th)):
+        img2_ascat[img_ascat >= th[i]] = i+1
+        img2_smos[img_smos >= th[i]] = i+1
+
+    fig = plt.figure(figsize=(20, 5))
+
+    fig.suptitle('Maximum drought level that is most likely at least as severe as observed (monthly average)')
     extent = [lons.min(), lons.max(), lats.min(), lats.max()]
     # cmap = 'viridis'
     cmap = get_cmap("magma", len(th)+1)
@@ -75,12 +150,76 @@ def plot_smos_predictability():
     ax.add_feature(cfeature.BORDERS, linewidth=0.5)
     img = ax.imshow(np.flipud(img2_smos), extent=extent, cmap=cmap, vmin=lim[0], vmax=lim[1],
                     transform=ccrs.PlateCarree())
-    cbar = fig.colorbar(img, orientation='vertical', ticks=np.arange(len(cl))+0.5)
+    cbar = fig.colorbar(img, ax=fig.axes, orientation='vertical', ticks=np.arange(len(cl))+0.5)
     cbar.ax.set_yticklabels(cl)
+
+    # plt.tight_layout()
+    # plt.show()
+
+    fout = r"H:\work\experiments\drought_predictability\most_likely_severity.png"
+    fig.savefig(fout, dpi=300, bbox_inches='tight')
+    plt.close()
+
+def plot_SNR():
+
+    avg_factor = np.sqrt(10)
+
+    if platform.system() == 'Darwin':
+        fname = '/Users/ag/Downloads/0-ERA5.swvl1_with_1-ASCAT.sm_with_2-SMOS_L2.Soil_Moisture.nc'
+    else:
+        fname = r"H:\work\experiments\drought_predictability\0-ERA5.swvl1_with_1-ASCAT.sm_with_2-SMOS_L2.Soil_Moisture.nc"
+
+    with Dataset(fname) as ds:
+        lats = ds['lat'][:].data
+        lons = ds['lon'][:].data
+        snr_ascat = ds['snr_1-ASCAT_between_0-ERA5_and_1-ASCAT_and_2-SMOS_L2'][:].data
+        snr_smos = ds['snr_2-SMOS_L2_between_0-ERA5_and_1-ASCAT_and_2-SMOS_L2'][:].data
+
+    latarr = np.arange(lats.min(), lats.max()+0.25, 0.25)
+    lonarr = np.arange(lons.min(), lons.max()+0.25, 0.25)
+
+    latidx = ((lats - lats.min())/0.25).astype('int')
+    lonidx = ((lons - lons.min())/0.25).astype('int')
+
+    img_ascat = np.full((len(latarr), len(lonarr)), np.nan)
+    img_smos = np.full((len(latarr), len(lonarr)), np.nan)
+    img_ascat[latidx, lonidx] = np.sqrt(10**(snr_ascat/10)) * avg_factor
+    img_smos[latidx, lonidx] = np.sqrt(10**(snr_smos/10)) * avg_factor
+
+    fig = plt.figure(figsize=(20, 6))
+    fig.suptitle('Maximum drought level that is most likely at least as severe as observed (monthly average)')
+    extent = [lons.min(), lons.max(), lats.min(), lats.max()]
+    cmap = 'viridis'
+    # cmap = get_cmap("magma", len(th)+1)
+    lim = [0, 5]
+    title1 = 'ASCAT'
+    title2 = 'SMOS'
+
+
+    ax = fig.add_subplot(1, 2, 1, projection=ccrs.PlateCarree())
+    ax.set_title(title1)
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.LAND, alpha=0.5)
+    ax.add_feature(cfeature.BORDERS, linewidth=0.5)
+    img = ax.imshow(np.flipud(img_ascat), extent=extent, cmap=cmap, vmin=lim[0], vmax=lim[1],
+                    transform=ccrs.PlateCarree())
+    # cbar = fig.colorbar(img, orientation='vertical', label=cmap_label, ticks=np.arange(len(cl))+0.5)
+    # cbar.ax.set_yticklabels(cl)
+
+    ax = fig.add_subplot(1, 2, 2, projection=ccrs.PlateCarree())
+    ax.set_title(title2)
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.LAND, alpha=0.5)
+    ax.add_feature(cfeature.BORDERS, linewidth=0.5)
+    img = ax.imshow(np.flipud(img_smos), extent=extent, cmap=cmap, vmin=lim[0], vmax=lim[1],
+                    transform=ccrs.PlateCarree())
+    cbar = fig.colorbar(img, orientation='vertical')
+    # cbar.ax.set_yticklabels(cl)
 
     plt.tight_layout()
     plt.show()
 
-
 if __name__=='__main__':
-    plot_smos_predictability()
+    plot_min_detection_threshold()
+    # plot_most_likely_threshold()
+    # plot_SNR()
